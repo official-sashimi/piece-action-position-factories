34214676f6cc1181b5663836beeecb4db1b245dd
Johndoe <56298669+togane@users.noreply.github.com>
2022-03-04 20:40:02 +0900

    install and initialize prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "arrowParens": "always",
+  "jsxSingleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true
+}
